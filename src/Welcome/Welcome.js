31c67d642d5f912317befaa77b967226e7c6ee7c
dj-neza <jacksparowiscool>
2019-02-21 10:24:07 +0100

    localStorage2

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -3,13 +3,19 @@ import { Link } from "react-router-dom";
 import "./Welcome.css";
 
 class Welcome extends Component {
+
+  testFn = () => {
+    localStorage.setItem("numOfGuests", 1);
+    localStorage.setItem("menu", JSON.stringify({"lala": 1, "lala2": 3}));
+  }
+
   render() {
     return (
       <div className="Welcome">
         <p>Welcome to the dinner planner React Startup code!</p>
 
         <Link to="/search">
-          <button>Start planning</button>
+          <button onClick={this.testFn}>Start planning</button>
         </Link>
       </div>
     );
